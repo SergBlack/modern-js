126448abd459288234291a673c6bcb0310ec6768
Осипчук Сергей Николаевич <sergei_osipchuk@epam.com>
2021-06-06 18:00:24 +0300

    iterators and generators

--- app/09.iterators.js
+++ app/09.iterators.js
@@ -13,7 +13,27 @@ console.log('Topic: Iterators');
 //   orange: '#f50',
 //   pink: '#e07'
 // };
-
+//
+// const keyValueIterable = (target) => {
+//   target[Symbol.iterator] = () => {
+//     const keys = Object.keys(target);
+//
+//     return {
+//       next() {
+//         const done = keys.length === 0;
+//         const key = keys.shift();
+//
+//         return {
+//           value: [key, target[key]],
+//           done,
+//         }
+//       }
+//     }
+//   };
+//
+//   return target;
+// }
+//
 // const itColors = keyValueIterable(colors);
 // for (const [, color] of itColors) {
 //   console.log(color);
@@ -24,7 +44,7 @@ console.log('Topic: Iterators');
 // RU: В коллекции хранятся все имена пользователей, которые присоединились к определенной группе телеграмм.
 //     Булевый флаг указывает, является ли пользователь администратором группы.
 //     Создайте итератор, который возвращает только имена администраторов.
-// EN: The following collection store all the names of the user that have joined a particular telegram group. 
+// EN: The following collection store all the names of the user that have joined a particular telegram group.
 //     The boolean flag indicates whether a user is an administrator of the group.
 //     Сreatereate an iterator that returns only the administrators' names.
 
@@ -36,7 +56,24 @@ console.log('Topic: Iterators');
 //   elena: false,
 //   felix: true,  // admin
 // };
-// [...users].forEach(name => console.log(name)); // boris, felix 
+//
+// users[Symbol.iterator] = function () {
+//   const entries = Object.entries(this).filter(([_, value]) => value);
+//
+//   return {
+//     next() {
+//       const done = entries.length === 0;
+//       const entry = entries.shift();
+//
+//       return {
+//         value: done ? undefined : entry[0],
+//         done,
+//       }
+//     }
+//   }
+// };
+//
+// [...users].forEach(name => console.log(name)); // boris, felix
 
 
 // Task 3
@@ -52,7 +89,27 @@ console.log('Topic: Iterators');
 //     })
 //   })
 // };
-
+//
+// const take = (sequence, amount) => {
+//   return {
+//     [Symbol.iterator]() {
+//       const iterator = sequence[Symbol.iterator]();
+//
+//       return {
+//         next() {
+//           if (amount-- < 1) {
+//             return {
+//               done: true,
+//               value: undefined,
+//             }
+//           }
+//           return iterator.next();
+//         }
+//       }
+//     }
+//   }
+// };
+//
 // const a = [...take(random, 3)];
 // console.log(a);
 
@@ -63,6 +120,34 @@ console.log('Topic: Iterators');
 // EN: Create iterable iterator, which produces Fibonacci numbers
 //     Implement method return, which allows you to stop iterator using for-of + break
 
+// const fibIterator = {
+//   [Symbol.iterator]: () => {
+//     const done = false;
+//     let prevValue = [...Array(2).keys()];
+//
+//     return {
+//       next: () => {
+//         const value = prevValue[0] + prevValue[1];
+//
+//         prevValue = [prevValue[1], value];
+//
+//         return { value, done };
+//       }
+//     }
+//   }
+// }
+//
+// const Fib = {
+//   [Symbol.iterator]: () => {
+//     const fibIter = fibIterator[Symbol.iterator]();
+//
+//     return {
+//       next: fibIter.next,
+//       return: () => ({ done: true }),
+//     }
+//   }
+// }
+//
 // for (let v of Fib) {
 //   console.log(v);
 //   if (v > 50) break;
@@ -75,5 +160,28 @@ console.log('Topic: Iterators');
 // EN: Create iterator for numbers, which allows you to get arrays of sequential integers.
 //     Example, [...-3] => [0, -1, -2, -3], [...3] => [0, 1, 2, 3]
 
+// Number.prototype[Symbol.iterator] = function () {
+//   const number = this.valueOf();
+//   const isNegative = -1 === Math.sign(number);
+//   const limit = Math.abs(number);
+//   let index = 0;
+//
+//   return {
+//     next: () => {
+//       const done = limit < index;
+//       let currentValue = index;
+//
+//       if (isNegative && currentValue !== 0) {
+//         currentValue = -currentValue;
+//       }
+//
+//       index += 1;
+//
+//       return { value: currentValue, done };
+//     }
+//
+//   }
+// }
+//
 // console.log([...-5]);
 // console.log([...5]);
--- app/10.sync-generators.js
+++ app/10.sync-generators.js
@@ -1,48 +1,109 @@
 console.log('Topic: Sync-Generators');
 
-// Task 1. 
+// Task 1.
 // RU: Наишите функцию-генератор generateSequence, которая генерирует числа в диапазоне от start до end.
 // EN: Create function-generator generateSequence, which generates and returns numbers from start to end.
 
+// function* generateSequence(start, end) {
+//   while (start <= end){
+//     yield start++;
+//   }
+// }
+//
+// console.log(...generateSequence(1, 10));
 
 // Task 2. Long Sequences
-// RU: Создать массив из чисел от 1 до 1000. 
+// RU: Создать массив из чисел от 1 до 1000.
 //     Реализовать функцию-генератор для формирования всех пар из этого массива.
-//     Генератор возвращает одну пару за один раз.  
+//     Генератор возвращает одну пару за один раз.
 //     Результат [1, 1], [1, 2], ..., [2, 1], [2, 2], ..., [1000, 1000]
 // EN: Create an array of numbers from 1 to 1000.
 //     Implement a generator function to create all pairs from this array.
 //     Result [1, 1], [1, 2], ..., [2, 1], [2, 2], ..., [1000, 1000]
 
+// const numbers = Array.from(Array(1000).keys());
+//
+// function* generatePair() {
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     for (let j = 0; j < numbers.length; j += 1) {
+//       yield [numbers[i], numbers[j]]
+//     }
+//   }
+// }
+//
+// const itGeneratePair = generatePair();
+//
+// console.log(itGeneratePair.next());
+// console.log(itGeneratePair.next());
+// console.log(itGeneratePair.next());
 
-// Task 3. 
+// Task 3.
 // RU: Реализуйте функцию генерации чисел Фибоначчи до бесконечности
 // EN: Implement the infinite function-generator of generating Fibonacci numbers
 
+// function* generateFib() {
+//   let firstNum = 0;
+//   let secondNum = 1;
+//
+//   while (true){
+//     const nextNum = secondNum + firstNum;
+//     firstNum = secondNum;
+//     secondNum = nextNum;
+//
+//     yield nextNum;
+//   }
+// }
+//
+// const itFib = generateFib();
+//
+// console.log(itFib.next().value);
+// console.log(itFib.next().value);
+// console.log(itFib.next().value);
+// console.log(itFib.next().value);
+// console.log(itFib.next().value);
+// console.log(itFib.next().value);
 
-// Task 4. yield* 
-// RU: Реализуйте функцию-генератор, которая будет генерировать последовательность 
+// Task 4. yield*
+// RU: Реализуйте функцию-генератор, которая будет генерировать последовательность
 //     символов от 0 до 9, затем от a до z, затем от A до Z. Воспользуйтесь функцией генератором
 //     generateSequence, реализованой выше, String.fromCharCode(code), и диапазоном чисел от 48 до 57,
 //     от 65 до 90 и от 97 до 122
 // EN: Create a function-generator that will generate a sequence
-//     characters from 0 to 9, then from a to z, then from A to Z. 
-//     Use the function-generator generateSequence implemented above, 
+//     characters from 0 to 9, then from a to z, then from A to Z.
+//     Use the function-generator generateSequence implemented above,
 //     String.fromCharCode (code), and a range of numbers from 48 to 57,
 //     65 to 90 and 97 to 122
 
+// function* generateSequence(start, end) {
+//   while (start <= end){
+//     yield start++;
+//   }
+// }
+//
+// function* outerGenerator() {
+//   yield* generateSequence(48, 57);
+//   yield* generateSequence(65, 90);
+//   yield* generateSequence(97, 122);
+// }
+//
+// let s = '';
+// for (let code of outerGenerator()) {
+//   s += String.fromCharCode(code);
+// }
+//
+// console.log(s);
 
 // Task 5. Sync Piping
-// RU: Создайте-функцию генератор multipleByTwo, которая принимает массив чисел, 
+// RU: Создайте-функцию генератор multipleByTwo, которая принимает массив чисел,
 //     умножает каждое число на 2 и возвращает их по одному
-//     Создайте-функцию генератор minusOne, которая принимает массив чисел, 
+//     Создайте-функцию генератор minusOne, которая принимает массив чисел,
 //     отнимает 1 и возвращает их по одному
-//     Создайте-функцию генератор isMultipleOfThree, которая принимает массив чисел, 
+//     Создайте-функцию генератор isMultipleOfThree, которая принимает массив чисел,
 //     и возвращает только те, которые делятся на 3 без остатка по одному
 //     Объявите массив от 0 до 9
 //     Вызовите первую функцию и передайте ей массив, результат этой функции передайте второй
 //     а затем результат второй передайтетретей.
-//     Примените спред оператор к резульату третей функции и создайте массив тех чисел, 
+//     Примените спред оператор к резульату третей функции и создайте массив тех чисел,
 //     которые останутся
 // EN: Create a function-generator multipleByTwo that takes an array of numbers,
 //     multiplies each number by 2 and returns them one at a time
@@ -51,34 +112,78 @@ console.log('Topic: Sync-Generators');
 //     Create a function-generator isMultipleOfThree that takes an array of numbers,
 //     and returns only those that are divided by 3 without remain part one at a time
 //     Declare an array from 0 to 9
-//     Call the first function and pass an array to it, pass the result of this function 
+//     Call the first function and pass an array to it, pass the result of this function
 //     to the second and then the result of the second pass to the third function.
-//     Apply the spread operator to the result of the third function and create an array 
+//     Apply the spread operator to the result of the third function and create an array
 //     of those numbers which will remain
 
+// function* multipleByTwo(numbers) {
+//   for (const number of numbers) {
+//     yield number * 2;
+//   }
+// }
+//
+// function* minusOne(numbers) {
+//   for (const number of numbers) {
+//     yield number - 1;
+//   }
+// }
+//
+// function* isMultipleOfThree(numbers) {
+//   for (const number of numbers) {
+//     if (number % 3 === 0) {
+//       yield number;
+//     }
+//   }
+// }
+//
+// const arrOfNum = Array.from(Array(10).keys());
+//
+// console.log([...isMultipleOfThree(minusOne(multipleByTwo(arrOfNum)))]);
 
 // Task 6. Communication with Infinite Generator
 // RU: Реализуйте бесконечную функцию-генератор calculation, которая будет выводить выражения
 //     a + b = ?, где a, b будет случайно генерировать и запрашивать у пользователя ответ.
-//     Если ответ верный, то писать в консоль true, иначе false. 
+//     Если ответ верный, то писать в консоль true, иначе false.
 //     Запустите код в консоли браузера
 // EN: Implement an infinite calculation generator-function that will output expressions
 //     a + b = ?, where a, b will be randomly generated and request a response from the user.
 //     If the answer is correct, then write true to the console, otherwise false.
 
+// const getRandomNumber = (limit) => Math.floor(Math.random() * limit);
+//
+// function* calculation() {
+//   const a = getRandomNumber(10);
+//   const b = getRandomNumber(10);
+//
+//   try {
+//     yield `${a} + ${b} = ?`;
+//     let answer = prompt('Ваш ответ?','0');
+//     yield answer;
+//
+//     (a + b) === Number(answer) ? console.log(true) : console.log(false);
+//   } catch (e) {
+//     console.log('Error');
+//   }
+// }
+//
+// let itCalculation = calculation();
+// console.log(itCalculation.next());
+// // itCalculation.throw('Error');
+// console.log(itCalculation.next());
+// console.log(itCalculation.next());
 
 // Task 7. throw method
-// RU: Модифицируйте код предыдущего задания так, чтобы при вызове it.throw() генератор 
+// RU: Модифицируйте код предыдущего задания так, чтобы при вызове it.throw() генератор
 //     не прекращал свою работу
 // EN: Modify the code of the previous task so that when it.throw() is called, the generator
 //     did not stop its work
 
-
-// Task 8. 
-// RU: В коллекции хранятся все имена пользователей, которые присоединились 
+// Task 8.
+// RU: В коллекции хранятся все имена пользователей, которые присоединились
 //     к определенной группе телеграмм.
 //     Булевый флаг указывает, является ли пользователь администратором группы.
-//     Создайте генератор, который на вход получает этот объект и 
+//     Создайте генератор, который на вход получает этот объект и
 //     возвращает только имена администраторов по одному за вызов.
 // EN: The collection stores all the usernames that joined
 //     to a specific telegram group.
@@ -94,3 +199,13 @@ console.log('Topic: Sync-Generators');
 //   elena: false,
 //   felix: true,  // admin
 // };
+//
+// function* adminGenerator(users) {
+//   const admins = Object.keys(users).filter((key) => users[key]);
+//
+//   for (const admin of admins) {
+//     yield admin;
+//   }
+// }
+//
+// console.log(...adminGenerator(users));
